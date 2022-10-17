894d8d36d7ab6a7057ab28348503c78dc1b60c6f
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-10-17 13:21:14 +0530

    Update DuplicateElementOfArray.java

--- src/com/bridgelabz/DuplicateElementOfArray.java
+++ src/com/bridgelabz/DuplicateElementOfArray.java
@@ -4,7 +4,7 @@ public class DuplicateElementOfArray {
     public static void main(String[] args) {
         int array[]=new int[]{10,20,30,40,50,60,70,80,90,100};
 
-        for(int i=0;i< array.length;i=i+1){
+        for(int i=0;i< array.length;i=i++){
             System.out.println(array[i]);
             System.out.println(array[i]);
         }
